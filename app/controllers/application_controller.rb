b7b710128afe78ca431948b7eb2400cc248aa113
Eddie Quan <eddieq@nulogy.com>
2016-03-04 16:25:10 -0500

    Removes unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,16 +12,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def error(e)
-    if true
-      error_info = {
-        :error => "internal-server-error",
-        :exception => "#{e.class.name} : #{e.message}",
-      }
-      error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
-      render :json => error_info.to_json, :status => 500
-    else
-      #render :text => "500 Internal Server Error", :status => 500 # You can render your own template here
-      raise e
-    end
+    error_info = {
+      :error => "internal-server-error",
+      :exception => "#{e.class.name} : #{e.message}",
+    }
+    error_info[:trace] = e.backtrace[0,10] if Rails.env.development?
+    render :json => error_info.to_json, :status => 500
   end
 end
